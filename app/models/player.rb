bc7388e783321fb0f8a55805266e0a719ddf60c4
Joshua Dance <joshua.dance@gmail.com>
2015-02-17 14:31:40 -0700

    added image uploads via paperclip

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,6 @@
 class Player < ActiveRecord::Base
+
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"] #paperclip requires
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217210804) do
+ActiveRecord::Schema.define(version: 20150217212614) do
 
   create_table "players", force: :cascade do |t|
     t.string   "name"
     t.integer  "wins"
     t.integer  "matches"
     t.float    "win_percentage"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
