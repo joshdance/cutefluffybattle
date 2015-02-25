c5f423123a4f1e2fb23c8903b38a87a87d581a82
Joshua Dance <joshua.dance@gmail.com>
2015-02-25 14:15:19 -0700

    trying to fix heroku

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -19,6 +19,9 @@ class PlayersController < ApplicationController
   # GET /players/new
   def new
     #this makes the new pin
+
+    @user = current_user
+
     @player = current_user.players.build
   end
 
