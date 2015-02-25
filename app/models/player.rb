cb0dfca0c3a79b8e94571ef11804ea55dac632a1
Joshua Dance <joshua.dance@gmail.com>
2015-02-24 20:07:24 -0700

    made name required to register a user, and image and name required to register a player

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,9 @@ class Player < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"] #paperclip requires
+	validates :image, presence: true
+    validates :name, presence: true
+
 
 	belongs_to :user
 	
